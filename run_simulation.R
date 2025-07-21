5e8799f754af4651dfc92d8a3b3f71ee133b3d6f
Jasper Arends <arends.jrm@gmail.com>
2025-07-21 14:59:16 +0200

    Update run_simulation.R

--- run_simulation.R
+++ run_simulation.R
@@ -10,8 +10,8 @@ source("main_estimators.R")
 
 # Define parameters
 N <- 250 # Sample sizes
-p1 <- 0.4 # P(X = 0)
-p2 <- 0.9 # P(Y = 0)
+p1 <- 0.2 # P(X = 0)
+p2 <- 0.6 # P(Y = 0)
 alpha <- 0 # Copula parameter
 sim <- 1000 # Number of simulations
 
@@ -20,3 +20,30 @@ results <- corr_bzid_simulation_main(N, sim, alpha, p1, p2)
 
 # Compute statistics
 stat <- corr_bzid_simulation_stats(results, alpha, p1, p2)
+
+# Plot results
+library(ggplot2)
+library(gridExtra)
+
+grid.arrange(
+  # Gini's gamma
+  ggplot(results) +
+    geom_boxplot(aes(y=Gini_gamma), outlier.size=.2, linewidth=.1) +
+    geom_hline(yintercept=as.double(stat$True[1]),
+               col='red', linetype='dashed') +
+    theme_classic(),
+  
+  # Spearman's footrule
+  ggplot(results) +
+    geom_boxplot(aes(y=Spm_foot), outlier.size=.2, linewidth=.1) +
+    geom_hline(yintercept=as.double(stat$True[2]),
+               col='red', linetype='dashed') +
+    theme_classic(),
+  
+  # Spearman's rho
+  ggplot(results) +
+    geom_boxplot(aes(y=Spm_rho), outlier.size=.2, linewidth=.1) +
+    geom_hline(yintercept=as.double(stat$True[3]),
+               col='red', linetype='dashed') +
+    theme_classic()
+)
