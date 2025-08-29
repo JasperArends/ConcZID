08407d9340d4aacd516b247587bc89a65d58d776
Jasper Arends <arends.jrm@gmail.com>
2025-08-29 15:28:59 +0200

    Update main_estimators.R
    Adjustments for new definitions

--- main_estimators.R
+++ main_estimators.R
@@ -9,24 +9,53 @@
 ########################################
 
 ####################
+# GINI'S GAMMA
+####################
+gini_gamma.est <- function(X, Y) {
+  # Ensure X and Y are numeric vectors
+  if (!is.numeric(X) || !is.numeric(Y)) {
+    stop("X and Y must be numeric vectors.")
+  }
+  
+  # Number of observations
+  N <- length(X)
+  if (length(Y) != N) {
+    stop("X and Y must have the same length.")
+  }
+  
+  # Concordance w.r.t. M and W
+  Q.HM <- QM.est(X, Y)
+  Q.HW <- QW.est(X, Y)
+  
+  # Centering
+  Q.P <- spm_rho.bound_est(X, Y)
+  
+  return ( Q.HM + Q.HW - (Q.P$up + Q.P$lw)/3 )
+}
+
+####################
 # SPEARMAN'S
 # FOOTRULE
 ####################
 spm_foot.est <- function(X, Y) {
-  Q.HM <- QM.est(X, Y)
-  Q.MM <- (1 - max(sum(X==0)/length(X),sum(Y==0)/length(Y))^2)
+  # Ensure X and Y are numeric vectors
+  if (!is.numeric(X) || !is.numeric(Y)) {
+    stop("X and Y must be numeric vectors.")
+  }
   
-  return ( (3 * Q.HM - Q.MM)/2 )
-}
-
-####################
-# GINI'S GAMMA
-####################
-gini_gamma.est <- function(X, Y) {
-  Q.HM <- QM.est(X, Y)
-  Q.HW <- QW.est(X, Y)
+  # Number of observations
+  N <- length(X)
+  if (length(Y) != N) {
+    stop("X and Y must have the same length.")
+  }
   
-  return ( Q.HM + Q.HW )
+  # Concordance w.r.t. M
+  Q.HM <- QM.est(X, Y)
+  
+  # Centering
+  Q.PM <- 1 - max(sum(X==0)/N,sum(Y==0)/N)^3
+  
+  return ( 0.5 * (3 * Q.HM - Q.PM) )
 }
 
 ####################
@@ -50,13 +79,11 @@ spm_rho.est <- function(X, Y) {
   p10 <- sum(X >  0 & Y == 0) / N  # P(X > 0, Y = 0)
   p11 <- sum(X >  0 & Y >  0) / N   # P(x > 0, Y > 0)
   
-  ######################################
   # Split data
   D10 <- which(X >  0 & Y == 0)
   D01 <- which(X == 0 & Y >  0)
   D11 <- which(X >  0 & Y >  0)
   
-  ######################################
   # Compute probabilities
   p1s <- ifelse(length(D10) > 0 & length(D11) > 0,  # P[X10 > X11]
                 mean(outer(X[D10], X[D11], FUN = ">")),
@@ -65,9 +92,8 @@ spm_rho.est <- function(X, Y) {
                 mean(outer(Y[D01], Y[D11], FUN = ">")),
                 0)
   
-  ######################################
   # Spearman's rho for positive values
-  rho11 <- ifelse(length(D11) > 0, cor(X[D11], Y[D11], method="spearman"), 0)
+  rho11 <- ifelse(length(D11) > 1, cor(X[D11], Y[D11], method="spearman"), 0)
   rho10 <- ifelse(length(D10) > 0 & length(D11) > 0,
                   12 * mean( colMeans(outer(X[D10], X[D11], FUN = "<")) *
                                colMeans(outer(Y[D11], Y[D11], FUN = "<"))) - 6 * (1 - p1s),
@@ -76,12 +102,11 @@ spm_rho.est <- function(X, Y) {
                   12 * mean( colMeans(outer(X[D11], X[D11], FUN = "<")) *
                                colMeans(outer(Y[D01], Y[D11], FUN = "<"))) - 6 * (1 - p2s),
                   0)
-  rho00 <- ifelse(length(D10) > 0 & length(D01) > 0,
+  rho00 <- ifelse(length(D11) > 0 & length(D10) > 0 & length(D01) > 0,
                   12 * mean( colMeans(outer(X[D10], X[D11], FUN = ">")) *
                                colMeans(outer(Y[D01], Y[D11], FUN = ">")) ) + 3 - 6 * (p1s + p2s),
                   0)
   
-  ######################################
   # Estimate Spearman's rho
   rho <- p11^3 * rho11 + p11^2 * p10 * rho10 + p11^2 * p01 * rho01 + p11 * p10 * p01 * rho00 +
     3 * (p11 * p00 - p10 * p01) + 3 * p11 * (p10 * (1 - 2 * p1s) + p01 * (1 - 2 * p2s))
@@ -99,12 +124,13 @@ QM.est <- function(X, Y) {
     stop("X and Y must be numeric vectors.")
   }
   
+  # Number of observations
   N <- length(X)
   if (length(Y) != N) {
     stop("X and Y must have the same length.")
   }
-
-  # Assume p1 <= p2
+  
+  # Ensure p1 <= p2
   if (sum(X == 0) > sum(Y == 0)) {
     Z <- X
     X <- Y
@@ -118,7 +144,7 @@ QM.est <- function(X, Y) {
   p11 <- sum(X >  0 & Y >  0)/N
   
   p1 <- sum(X == 0)/N
-  p2 <- sum(Y == 0)/N # Assume p2 >= p1
+  p2 <- sum(Y == 0)/N
   
   # Divide into p11(I), p11(II), p10(I) and p01(I)
   Q11I <- (X > 0 & rank(X, ties.method="max")/N <= p2 & Y >  0) # X > 0, Y > 0 and F(X) <= p2
@@ -160,12 +186,13 @@ QW.est <- function(X, Y) {
     stop("X and Y must be numeric vectors.")
   }
   
+  # Number of observations
   N <- length(X)
   if (length(Y) != N) {
     stop("X and Y must have the same length.")
   }
-
-  # Assume p1 <= p2
+  
+  # Ensure p1 <= p2
   if (sum(X == 0) > sum(Y == 0)) {
     Z <- X
     X <- Y
@@ -183,6 +210,7 @@ QW.est <- function(X, Y) {
   
   if ( p1 + p2 < 1 ) {
     
+    # Split observations
     QI <- X > 0 & rank(X, ties.method="max")/N <= 1 - p2
     QII <- rank(X, ties.method="max")/N > 1 - p2
     QA <- rank(Y, ties.method="max")/N > 1 - p1
@@ -225,6 +253,7 @@ QW.est <- function(X, Y) {
   }
   
   else { # p1 + p2 > 1
+    # Split observations
     Q1m <- X > 0
     Qm1 <- Y > 0
     Q11 <- Q1m & Qm1
@@ -239,6 +268,7 @@ QW.est <- function(X, Y) {
     stat <- c(mean(Sp[Q11]), mean(Rp[Q11]))
     stat[is.na(stat)] <- 0
     
+    # Compute concordance
     QHW <- p11 * (1 - p1) * (2 * stat[2] - 1) +
       p11 * (1 - p2) * (2 * stat[1] - 1) +
       p11 * (p1 + p2 - 1) - p10 * (1 - p2) - p01 * (1 - p1)
@@ -248,8 +278,62 @@ QW.est <- function(X, Y) {
 }
 
 ########################################
-# BOUNDS
+# THEORETICAL BOUNDS
 ########################################
+
+####################
+# GINI'S GAMMA
+####################
+gini_gamma.bound <- function(p1, p2) {
+  # Upper bound
+  gamma.up <- ifelse(p1 + p2 >= 1,
+                     2/3 + 1/3 * max(p1,p2) * (3 - 6 * max(p1,p2) + max(p1,p2)^2),
+                     1 - max(p1,p2)^2 - 1/3 * min(p1,p2)^3)
+  
+  # Lower bound
+  gamma.lw <- ifelse(p1 + p2 >= 1,
+                     (1 - max(p1,p2)) * (p1 + p2 + min(p1,p2) - 2) - 1/3 * (1 - max(p1,p2)^3),
+                     (1 - p1 - p2)^2 - 2 * (1 - p1)*(1 - p2) - 1/3 * min(p1,p2)^3)
+  
+  return (list(up=gamma.up, lw=gamma.lw))
+}
+
+####################
+# SPEARMAN'S
+# FOOTRULE
+####################
+spm_foot.bound <- function(p1, p2) {
+  # Upper bound
+  spm_foot.up <- 1 - 3/2 * max(p1,p2)^2 + 1/2 * max(p1,p2)^3
+  
+  # Lower bound
+  spm_foot.lw <- 3/2 * max(0, p1 + p2 - 1) * (1 - max(p1,p2)) - 1/2 * (1 - max(p1,p2)^3)
+  
+  return (list(up=spm_foot.up, lw=spm_foot.lw))
+}
+
+####################
+# SPEARMAN'S RHO
+####################
+spm_rho.bound <- function(p1, p2) {
+  # Upper bound
+  spm_rho.up <- 1 - max(p1,p2)^3
+  
+  # Lower bound
+  spm_rho.lw <- ifelse(p1 + p2 <= 1,
+                       p1^3 + p2^3 - 1,
+                       -3 * (1 - p1) * (1 - p2))
+  
+  return (list(up=spm_rho.up, lw=spm_rho.lw))
+}
+
+########################################
+# ESTIMATED BOUNDS
+########################################
+
+####################
+# GINI'S GAMMA
+####################
 gini_gamma.bound_est <- function(X, Y) {
   # Ensure X and Y are numeric vectors
   if (!is.numeric(X) || !is.numeric(Y)) {
@@ -261,18 +345,18 @@ gini_gamma.bound_est <- function(X, Y) {
   p2 <- sum(Y == 0)/length(Y) # P[Y = 0]
   
   # Upper bound
-  gamma.up <- ifelse(p1 + p2 >= 1,
-                     (1 - max(p1,p2)) * (p1 + p2 + max(p1,p2)),
-                     (1 - max(p1,p2)^2))
+  gamma.up <- gini_gamma.bound(p1, p2)$up
   
   # Lower bound
-  gamma.lw <- ifelse(p1 + p2 >= 1,
-                     max(p1,p2) - max(p1,p2)^2 - 3 * (1 - p1)*(1 - p2),
-                     (1 - p1 - p2)^2 - 2 * (1 - p1) * (1 - p2))
+  gamma.lw <- gini_gamma.bound(p1, p2)$lw
   
-  return (list(up=gamma.up, lw=gammal.w))
+  return (list(up=gamma.up, lw=gamma.lw))
 }
 
+####################
+# SPEARMAN'S
+# FOOTRULE
+####################
 spm_foot.bound_est <- function(X, Y) {
   # Ensure X and Y are numeric vectors
   if (!is.numeric(X) || !is.numeric(Y)) {
@@ -284,16 +368,17 @@ spm_foot.bound_est <- function(X, Y) {
   p2 <- sum(Y == 0)/length(Y) # P[Y = 0]
   
   # Upper bound
-  spm_foot.up <- 1 - max(p1,p2)^2
+  spm_foot.up <- spm_foot.bound(p1, p2)$up
   
   # Lower bound
-  spm_foot.lw <- ifelse(p1 + p2 >= 1,
-                        (3 * max(p1,p2) - 2 * max(p1,p2)^2 - 3 * (1 - p1) * (1 - p2) - 1)/2,
-                        - (1 - max(p1,p2)^2)/2)
+  spm_foot.lw <- spm_foot.bound(p1, p2)$lw
   
   return (list(up=spm_foot.up, lw=spm_foot.lw))
 }
 
+####################
+# SPEARMAN'S RHO
+####################
 spm_rho.bound_est <- function(X, Y) {
   # Ensure X and Y are numeric vectors
   if (!is.numeric(X) || !is.numeric(Y)) {
@@ -305,12 +390,10 @@ spm_rho.bound_est <- function(X, Y) {
   p2 <- sum(Y == 0)/length(Y) # P[Y = 0]
   
   # Upper bound
-  spm_rho.up <- 1 - max(p1,p2)^3
+  spm_rho.up <- spm_rho.bound(p1, p2)$up
   
   # Lower bound
-  spm_rho.lw <- ifelse(p1 + p2 <= 1,
-                       p1^3 + p2^2 - 1,
-                       -3 * (1 - p1) * (1 - p2))
+  spm_rho.lw <- spm_rho.bound(p1, p2)$lw
   
   return (list(up=spm_rho.up, lw=spm_rho.lw))
 }
