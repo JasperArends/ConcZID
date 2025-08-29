307b61da216d095766a15c94f75b14310d05c094
Jasper Arends <arends.jrm@gmail.com>
2025-08-29 15:32:06 +0200

    Update simulation_functions.R
    Simulation functions for bounds estimation

--- simulation_functions.R
+++ simulation_functions.R
@@ -31,7 +31,7 @@ gen_sample <- function(N, alpha, p1, p2) {
 
 
 ####################
-# SIMULATIONS
+# MAIN ESTIMATORS
 ####################
 
 corr_bzid_simulation_main <- function(N, sim, alpha, p1, p2) {
@@ -45,15 +45,11 @@ corr_bzid_simulation_main <- function(N, sim, alpha, p1, p2) {
     stop("p1 and p2 must be numeric.")
   }
   
-  ####################
   # Create data frames
-  ####################
   results <- data.frame(matrix(nrow=0, ncol=4))
   colnames(results) <- c("Simulation", "Gini_gamma", "Spm_foot", "Spm_rho")
   
-  ####################
   # Perform simulation
-  ####################
   for (ix in 1:sim) {
     # Generate sample
     X <- gen_sample(N, alpha, p1, p2)
@@ -62,6 +58,7 @@ corr_bzid_simulation_main <- function(N, sim, alpha, p1, p2) {
     if (sum(X[,2] == 0) < sum(X[,1] == 0))
       X <- X[,c(2, 1)]
     
+    # Compute estimates
     results[ix,] <- c(ix,
                       gini_gamma.est(X[,1], X[,2]),
                       spm_foot.est(X[,1], X[,2]),
@@ -73,11 +70,9 @@ corr_bzid_simulation_main <- function(N, sim, alpha, p1, p2) {
 
 corr_bzid_simulation_stats <- function(results, alpha, p1, p2) {
   # Compute true values
-  conc_true <- list(gini_gamma = gini_gamma.indep(p1,p2) * (1 - abs(alpha)) +
-                      gini_gamma.bound(p1,p2)$up * alpha * (alpha > 0) -
+  conc_true <- list(gini_gamma = gini_gamma.bound(p1,p2)$up * alpha * (alpha > 0) -
                       gini_gamma.bound(p1,p2)$lw * alpha * (alpha < 0),
-                    spm_foot = spm_foot.indep(p1,p2) * (1 - abs(alpha)) +
-                      spm_foot.bound(p1,p2)$up * alpha * (alpha > 0) -
+                    spm_foot = spm_foot.bound(p1,p2)$up * alpha * (alpha > 0) -
                       spm_foot.bound(p1,p2)$lw * alpha * (alpha < 0),
                     spm_rho = spm_rho.bound(p1,p2)$up * alpha * (alpha > 0) -
                       spm_rho.bound(p1,p2)$lw * alpha * (alpha < 0))
@@ -120,59 +115,97 @@ corr_bzid_simulation_stats <- function(results, alpha, p1, p2) {
 }
 
 ####################
-# THEORETICAL BOUNDS
-####################
-gini_gamma.bound <- function(p1, p2) {
-  # Upper bound
-  gamma.up <- ifelse(p1 + p2 >= 1,
-                     (1 - max(p1,p2)) * (p1 + p2 + max(p1,p2)),
-                     (1 - max(p1,p2)^2))
-  
-  # Lower bound
-  gamma.lw <- ifelse(p1 + p2 >= 1,
-                     max(p1,p2) - max(p1,p2)^2 - 3 * (1 - p1)*(1 - p2),
-                     (1 - p1 - p2)^2 - 2 * (1 - p1) * (1 - p2))
-  
-  return (list(up=gamma.up, lw=gamma.lw))
-}
-
-spm_foot.bound <- function(p1, p2) {
-  # Upper bound
-  spm_foot.up <- 1 - max(p1,p2)^2
-  
-  # Lower bound
-  spm_foot.lw <- ifelse(p1 + p2 >= 1,
-                        (3 * max(p1,p2) - 2 * max(p1,p2)^2 - 3 * (1 - p1) * (1 - p2) - 1)/2,
-                        - (1 - max(p1,p2)^2)/2)
-  
-  return (list(up=spm_foot.up, lw=spm_foot.lw))
-}
-
-spm_rho.bound <- function(p1, p2) {
-  # Upper bound
-  spm_rho.up <- 1 - max(p1,p2)^3
-  
-  # Lower bound
-  spm_rho.lw <- ifelse(p1 + p2 <= 1,
-                       p1^3 + p2^3 - 1,
-                       -3 * (1 - p1) * (1 - p2))
-  
-  return (list(up=spm_rho.up, lw=spm_rho.lw))
-}
-
-####################
-# VALUES UNDER
-# INDEPENDENCE
+# BOUND ESTIMATORS
 ####################
 
-gini_gamma.indep <- function(p1, p2) {
-  spm_rho <- spm_rho.bound(p1, p2)
+bnd_bzid_simulation_main <- function(N, sim, p1, p2) {
+  # Ensure N and sim and alpha are numeric
+  if (!is.numeric(N) || !is.numeric(sim) || !is.numeric(alpha)) {
+    stop("N, sim and alpha must be numeric.")
+  }
   
-  return ( spm_rho$up/3 + spm_rho$lw/3 )
+  # Ensure p1 and p2 are numeric
+  if (!is.numeric(p1) || !is.numeric(p2)) {
+    stop("p1 and p2 must be numeric.")
+  }
+  
+  # Create data frames
+  results <- data.frame(matrix(nrow=0, ncol=7))
+  colnames(results) <- c("Simulation",
+                         "gamma_lw", "gamma_up",
+                         "foot_lw", "foot_up",
+                         "rho_lw", "rho_up")
+  
+  # Perform simulation
+  for (ix in 1:sim) {
+    # Generate sample
+    X <- gen_sample(N, 1, p1, p2)
+    
+    # Assume p1 <= p2
+    if (sum(X[,2] == 0) < sum(X[,1] == 0))
+      X <- X[,c(2, 1)]
+    
+    # Estimate inflation
+    p1.est <- sum(X[,1] == 0)/N
+    p2.est <- sum(X[,2] == 0)/N
+    
+    # Compute estimates
+    results[ix,] <- c(ix,
+                      gini_gamma.bound_est(X[,1], X[,2])$lw,
+                      gini_gamma.bound_est(X[,1], X[,2])$up,
+                      spm_foot.bound_est(X[,1], X[,2])$lw,
+                      spm_foot.bound_est(X[,1], X[,2])$up,
+                      spm_rho.bound_est(X[,1], X[,2])$lw,
+                      spm_rho.bound_est(X[,1], X[,2])$up)
+    
+  }
+  
+  return ( results )
 }
 
-spm_foot.indep <- function(p1, p2) {
-  spm_rho <- spm_rho.bound(p1, p2)
+bnd_bzid_simulation_stats <- function(results, p1, p2) {
+  # Compute true values
+  bnd_true <- list(gini_gamma.lw = gini_gamma.bound(p1,p2)$lw,
+                    gini_gamma.up = gini_gamma.bound(p1,p2)$up,
+                    spm_foot.lw = spm_foot.bound(p1,p2)$lw,
+                    spm_foot.up = spm_foot.bound(p1,p2)$up,
+                    spm_rho.lw = spm_rho.bound(p1,p2)$lw,
+                    spm_rho.up = spm_rho.bound(p1,p2)$up)
   
-  return ( (spm_rho$up - (1 - max(p1,p2)^2 ))/2 )
+  # Statistics
+  bnd_stat <- data.frame(matrix(nrow=3, ncol=7))
+  colnames(bnd_stat) <- c("Variable", "True lower", "True upper", "Mean lower", "Mean upper",
+                          "MSE lower", "MSE upper")
+  
+  # Gini's gamma
+  bnd_stat[1,] <- c("Gini's gamma",
+                     bnd_true$gini_gamma.lw,
+                     bnd_true$gini_gamma.up,
+                     mean(results$gamma_lw),
+                     mean(results$gamma_up),
+                     mse(bnd_true$gini_gamma.lw, results$gamma_lw),
+                     mse(bnd_true$gini_gamma.up, results$gamma_up)
+  )
+  
+  # Spearman's footrule
+  bnd_stat[2,] <- c("Spearman's footrule",
+                     bnd_true$spm_foot.lw,
+                     bnd_true$spm_foot.up,
+                     mean(results$foot_lw),
+                     mean(results$foot_up),
+                     mse(bnd_true$spm_foot.lw, results$foot_lw),
+                     mse(bnd_true$spm_foot.up, results$foot_up)
+  )
+  
+  # Spearman's rho
+  bnd_stat[3,] <- c("Spearman's rho",
+                     bnd_true$spm_rho.lw,
+                     bnd_true$spm_rho.up,
+                     mean(results$rho_lw),
+                     mean(results$rho_up),
+                     mse(bnd_true$spm_rho.lw, results$rho_lw),
+                     mse(bnd_true$spm_rho.up, results$rho_up)
+  )
+  
+  return ( bnd_stat )
 }
