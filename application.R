316a007009a2f897898fe0b4ccbe99c231352fa4
Jasper Arends <arends.jrm@gmail.com>
2026-04-17 18:48:27 +0200

    Update application.R

--- application.R
+++ application.R
@@ -22,17 +22,17 @@ danish <- data.frame("Contents" = danishmulti$Contents, "Profits"=danishmulti$Pr
 sum(danish$Contents == 0)/nrow(danish)
 sum(danish$Profits == 0)/nrow(danish)
 
-# Spearman's rho
-spm_rho.est(danish$Contents, danish$Profits)
-spm_rho.bound_est(danish$Contents, danish$Profits)
+# Gini's gamma
+gini_gamma.est(danish$Contents, danish$Profits)
+gini_gamma.bound_est(danish$Contents, danish$Profits)
 
 # Spearman's footrule
 spm_foot.est(danish$Contents, danish$Profits)
 spm_foot.bound_est(danish$Contents, danish$Profits)
 
-# Gini's gamma
-gini_gamma.est(danish$Contents, danish$Profits)
-gini_gamma.bound_est(danish$Contents, danish$Profits)
+# Spearman's rho
+spm_rho.est(danish$Contents, danish$Profits)
+spm_rho.bound_est(danish$Contents, danish$Profits)
 
 ########################################
 # PRECIPITATION DATASET
@@ -40,19 +40,22 @@ gini_gamma.bound_est(danish$Contents, danish$Profits)
 # The data is freely available from the Royal Netherlands Meteorological
 # Institute (KNMI) and can be downloaded at
 # https://www.knmi.nl/nederland-nu/klimatologie/uurgegevens. The following part
-# uses the precipitation (RH) from Schiphol (240) and De Bilt (260) during the
-# time period of 2020 until 2023.
+# uses the precipitation (RH, given in 10^-1 mm) from Schiphol (240) and
+# De Bilt (260) during the time period of 2021 until 2025.
 
 # Load data
-schiphol <- read.csv('uurgeg_240_2021-2030.txt', skip=30)
-debilt <- read.csv('uurgeg_260_2021-2030.txt', skip=30)
+schiphol <- read.csv('./Data sets/Precipitation/uurgeg_240_2021-2030.txt', skip=30) %>%
+  mutate(YYYYMMDD = as.Date(paste0(YYYYMMDD), format='%Y%m%d'))
+debilt <- read.csv('./Data sets/Precipitation/uurgeg_260_2021-2030.txt', skip=30) %>%
+  mutate(YYYYMMDD = as.Date(paste0(YYYYMMDD), format='%Y%m%d'))
 
-# Filter data from 2020 - 2023 and 12H00 - 13H00
+# Filter data from 2020 - 2025 and 12H00 - 13H00
 data <- data.frame(date = schiphol$YYYYMMDD,
                    hour = schiphol$HH,
                    schiphol = schiphol$RH,
                    debilt = debilt$RH) %>%
-  filter(date < 20240000, hour == 12)
+  filter(date < as.Date("20260101", format='%Y%m%d'), hour == 12,
+         weekdays(date) == "Monday")
 
 # Dataset entries with no rain are indicated by -1
 data[data < 0] <- 0
@@ -62,54 +65,20 @@ sum(data$schiphol == 0)/nrow(data)
 sum(data$debilt == 0)/nrow(data)
 
 # Add random noise to positive tied observations
-data$schiphol[data$schiphol > 0] <- data$schiphol[data$schiphol > 0] 
+data$schiphol[data$schiphol > 0] <- data$schiphol[data$schiphol > 0] +
   runif(sum(data$schiphol > 0))
-data$debilt[data$debilt] <- data$debilt[data$debilt > 0] +
+data$debilt[data$debilt > 0] <- data$debilt[data$debilt > 0] +
   runif(sum(data$debilt > 0))
 
-# Spearman's rho
-spm_rho.est(dt$schiphol, dt$debilt)
-spm_rho.bound_est(dt$schiphol, dt$debilt)
-
-# Spearman's footrule
-spm_foot.est(dt$schiphol, dt$debilt)
-spm_foot.bound_est(dt$schiphol, dt$debilt)
 
 # Gini's gamma
-gini_gamma.est(dt$schiphol, dt$debilt)
-gini_gamma.bound_est(dt$schiphol, dt$debilt)
+gini_gamma.est(data$schiphol, data$debilt)
+gini_gamma.bound_est(data$schiphol, data$debilt)
 
+# Spearman's footrule
+spm_foot.est(data$schiphol, data$debilt)
+spm_foot.bound_est(data$schiphol, data$debilt)
 
-# Illustrative figures
-ggplot(data/10, aes(x=debilt, y=schiphol)) +
-  geom_point(size=.4) +
-  labs(x="De Bilt", y="Schiphol") +
-  coord_fixed(ratio = 1, xlim=c(0, 11), ylim=c(0, 5)) +
-  scale_x_continuous(breaks=0:11, expand=expansion(mult = c(0.02, .05))) +
-  scale_y_continuous(breaks=0:5, expand=expansion(mult = c(0.05, 0.05))) +
-  theme_classic() +
-  theme(axis.line = element_line(linewidth=.2),
-        axis.ticks.x = element_line(linewidth=.2),
-        axis.ticks.y = element_line(linewidth=.2))
-
-ggplot(data/10, aes(x=debilt)) +
-  geom_histogram(aes(y=after_stat(count) / nrow(data)), breaks=c(0, seq(0.1, 11, 0.1))) +
-  labs(x="", y="Margin") +
-  xlim(0, 11) +
-  scale_y_continuous(limits=c(0, 1), breaks=c(0, .5, 1)) +
-  theme_classic() +
-  theme(axis.line = element_line(linewidth=.2),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_line(linewidth=.2))
-
-ggplot(data/10, aes(x=schiphol)) +
-  geom_histogram(aes(y=after_stat(count) / nrow(data)), breaks=c(0, seq(0.1, 5, 0.1))) +
-  labs(x="", y="Margin") +
-  scale_y_continuous(position="right", limits=c(0, 1), breaks=c(0, .5, 1)) +
-  scale_x_reverse() +
-  theme_classic() +
-  theme(axis.line = element_line(linewidth=.2),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_line(linewidth=.2))
+# Spearman's rho
+spm_rho.est(data$schiphol, data$debilt)
+spm_rho.bound_est(data$schiphol, data$debilt)
